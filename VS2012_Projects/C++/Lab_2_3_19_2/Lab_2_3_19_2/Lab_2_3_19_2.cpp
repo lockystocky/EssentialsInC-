79fdd2b1d0d7f37e672b9c36b677b5a7c51d0586
lockystocky <locky.stocky24@gmail.com>
2017-10-22 16:13:16 +0300

    first commit from bash

---
+++ VS2012_Projects/C++/Lab_2_3_19_2/Lab_2_3_19_2/Lab_2_3_19_2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(void) 
+{
+	double pi4 = 0.;
+	long n;
+	bool adding = true;
+	cout << "Number of iterations? ";
+	cin >> n;
+	for(long i = 0; i < n; i++)
+	{
+		if(adding) 
+		{
+			pi4 += 1.0/(2.0*(i + 1) - 1);
+			//cout << " + 1/" << (2*(i + 1) - 1) << endl;
+		}
+		else 
+		{
+			pi4 -= 1.0/(2*(i + 1) - 1);
+			//cout << " - 1/" << (2*(i + 1) - 1) << endl;
+		}
+		adding = (!adding);
+	}
+	cout.precision(20);
+	cout << "Pi = " << (pi4 * 4.) << endl;
+	cin.get();
+	cin.get();
+	return 0;
+}
